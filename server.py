96b748ddacecb80530594ef6aac7cf5084164e5c
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2024-11-20 20:18:10 +0700

    ANEH BET DIG BICK

--- server.py
+++ server.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     # Get the port from the environment variable (default to 8080 for Cloud Run)
     port = int(os.environ.get("PORT", 8080))
     # Use '0.0.0.0' as the host to allow external access in Cloud Run
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
