37d591bf951045fac1a34cf5681cb69420bff177
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2024-11-20 19:12:59 +0700

    adding feature

--- app/data/storage.py
+++ app/data/storage.py
@@ -1,13 +1,11 @@
 import os
 from google.cloud import storage
-from flask import jsonify
+import datetime
+from dotenv import load_dotenv
+load_dotenv()
 
-
-# Use with utmost fucking caution!!!
-# Highly discouraged to use this in production
-# This is for testing purposes only
-# Please me don't forget to remove this
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+bucket_name = os.environ.get("BUCKET_NAME")
 
 def get_bucket(bucket_name):
     client = storage.Client()
@@ -19,18 +17,12 @@ def upload_blob_to_folder(bucket_name, source_file_name, destination_folder_name
     blob = bucket.blob(f"{destination_folder_name}/{destination_blob_name}")
     blob.upload_from_filename(source_file_name)
     print(f"File {source_file_name} uploaded to {destination_folder_name}/{destination_blob_name}.")
+    
 
-def generate_signed_url(file_name):
-  
-    bucket_name = "matane-bucket"
+def generate_presigned_url_for_profile(bucket_name, blob_name, expiration=3600):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
+    blob = bucket.blob(f"userProfile/{blob_name}")
+    url = blob.generate_signed_url(expiration=datetime.timedelta(seconds=expiration), method='GET')
+    return url
 
-    signed_url = blob.generate_signed_url(
-        version="v4",
-        expiration=3600,  # URL valid for 1 hour
-        method="GET"
-    )
-
-    return jsonify({"signed_url": signed_url})
