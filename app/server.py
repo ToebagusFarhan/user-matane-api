86b2c1f8fa009c448e144d60196ec7dce1534b27
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2024-11-20 10:44:50 +0700

    small change

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 # server.py
+import os
 from flask import Flask
 from routes import user_routes
 
@@ -6,4 +7,7 @@ app = Flask(__name__)
 app.register_blueprint(user_routes)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=9000, debug=True)
+    # Get the port from the environment variable (default to 8080 for Cloud Run)
+    port = int(os.environ.get("PORT", 8080))
+    # Use '0.0.0.0' as the host to allow external access in Cloud Run
+    app.run(host='0.0.0.0', port=port)
