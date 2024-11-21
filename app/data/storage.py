a596a1caf383426e30eb96da11a80ff914abfcf1
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2024-11-21 10:12:46 +0700

    N try of presigned url

--- app/data/storage.py
+++ app/data/storage.py
@@ -1,18 +1,21 @@
 import os
-from google.cloud import storage
-from google.auth import compute_engine
 import datetime
+from google.cloud import storage
+from google.oauth2 import service_account
+
 from dotenv import load_dotenv
 load_dotenv()
 
+GOOGLE_APPLICATION_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    "/SECRETS/SERVICE_ACCOUNT"
+)
 
 bucket_name = os.environ.get("BUCKET_NAME")
 service_account = os.environ.get("SERVICE_ACCOUNT")
-credentials = compute_engine.Credentials()
 project_id = os.environ.get("PROJECT_ID")
 
 def get_bucket(bucket_name):
-    client = storage.Client(credentials=credentials, project=project_id)
+    client = storage.Client(credentials=GOOGLE_APPLICATION_CREDENTIALS)
     bucket = client.get_bucket(bucket_name)
     return bucket
 
@@ -26,6 +29,6 @@ def upload_blob_to_folder(source_file_name, destination_folder_name, destination
 def generate_presigned_url_for_profile(blob_name, expiration=3600):
     bucket = get_bucket(bucket_name)
     blob = bucket.blob(f"userProfile/{blob_name}")
-    url = blob.generate_signed_url(version="v4", expiration=datetime.timedelta(seconds=expiration), method='GET', service_account_email=service_account)
+    url = blob.generate_signed_url(version="v4", expiration=datetime.timedelta(seconds=expiration), method='GET')
     return url
 
