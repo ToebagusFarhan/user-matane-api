83a0ba790fad7db256f10695a525ed50b7d3bab1
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2024-11-21 11:36:31 +0700

    fixing api auth

--- app/utils/apiauth.py
+++ app/utils/apiauth.py
@@ -3,8 +3,13 @@ import os
 
 
 def amIAllowed():
-    STORED_API_KEY = os.environ.get('API_KEY')
-    api_key = request.headers.get('x-api-key')
-    if api_key and api_key == STORED_API_KEY:
-        return True
-    return False
+    try:
+        STORED_API_KEY = os.environ.get('API_KEY')
+        api_key = request.headers.get('x-api-key')
+        if api_key and api_key == STORED_API_KEY:
+            return True
+        return False
+    except Exception as e:
+        # Log the exception if needed
+        print(f"An error occurred: {e}")
+        return False
